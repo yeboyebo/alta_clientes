e1a3efe8520fb5f146d05463e745419ae4108836
andres.yeboyebo@gmail.com <andres@yeboyebo.es>
2018-12-17 09:54:57 +0100

    Parcheado script clientes

--- model_flfactppal__clientes.py
+++ model_flfactppal__clientes.py
@@ -1,56 +1,12 @@
-# @class_declaration interna_clientes #
-from YBUTILS.viewREST import helpers
-from models.flfactppal import models as modelos
-import importlib
-
-
-class interna_clientes(modelos.mtd_clientes, helpers.MixinConAcciones):
-    pass
-
-    class Meta:
-        proxy = True
-
 
 # @class_declaration alta_clientes_clientes #
-class alta_clientes_clientes(interna_clientes, helpers.MixinConAcciones):
+class alta_clientes_clientes(flfactppal_clientes, helpers.MixinConAcciones):
     pass
 
     class Meta:
         proxy = True
-
-    def initValidation(name, data=None):
-        return form.iface.initValidation(name, data)
-
-    def iniciaValoresLabel(self, template=None, cursor=None, data=None):
-        return form.iface.iniciaValoresLabel(self, template, cursor)
-
-    def bChLabel(fN=None, cursor=None):
-        return form.iface.bChLabel(fN, cursor)
-
-    def getFilters(self, name, template=None):
-        return form.iface.getFilters(self, name, template)
-
-    def getForeignFields(self, template=None):
-        return form.iface.getForeignFields(self, template)
-
-    def getDesc():
-        return form.iface.getDesc()
 
     @helpers.decoradores.accion(aqparam=["oParam"])
     def subirDocSepa(self, oParam):
         return form.iface.subirDocSepa(self, oParam)
 
-
-# @class_declaration clientes #
-class clientes(alta_clientes_clientes, helpers.MixinConAcciones):
-    pass
-
-    class Meta:
-        proxy = True
-
-
-definitions = importlib.import_module("models.flfactppal.clientes_def")
-form = definitions.FormInternalObj()
-form._class_init()
-form.iface.ctx = form.iface
-form.iface.iface = form.iface
